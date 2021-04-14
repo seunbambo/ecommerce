498567f46f41d66dc923b253d9c76332ab87a5d4
Oluwaseun Adebambo <seun.bambo@gmail.com>
2021-04-14 18:23:27 +0100

    Implemented firebase utils, included ability to store authenticated users into firestore database

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -5,11 +5,38 @@ import 'firebase/auth';
 const config = {
   apiKey: 'AIzaSyDKcAmRoGWtUC9qeilW-BIt9fcHihUuPhg',
   authDomain: 'crwn-db-fbce2.firebaseapp.com',
+  databaseURL: 'https://crwn-db-fbce2-default-rtdb.firebaseio.com',
   projectId: 'crwn-db-fbce2',
   storageBucket: 'crwn-db-fbce2.appspot.com',
   messagingSenderId: '150059958557',
   appId: '1:150059958557:web:3824e3f861b6f4d9d721f4',
   measurementId: 'G-QKM0G4P15E',
+};
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData,
+      });
+    } catch (error) {
+      console.log('error creating user', error.message);
+    }
+  }
+
+  return userRef;
 };
 
 firebase.initializeApp(config);
